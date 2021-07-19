cbc3fe245009384ebe689a5296eecad1276c1c77
yuzhang <zhangy@lianzhu.com>
2021-07-19 11:48:18 +0800

    :hammer: update description

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+# -*- coding: utf8 -*-
+
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 def read():
@@ -8,12 +14,14 @@ def read():
 
 setup(
     name='flask_jaeger',
-    version='1.0.0',
+    version='1.0.1',
     packages=['flask_jaeger', ],
     url='',
     license='',
     author='yuzhang',
     author_email='geasyheart@163.com',
     description='flask jaeger',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=read()
 )
